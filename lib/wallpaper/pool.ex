f204e6d4f9dc488c4e138b71fdfa1d154bed053c
sa <sanami77@gmail.com>
2023-10-06 23:34:41 +0600

    Limit pool transaction tasks

--- lib/wallpaper/pool.ex
+++ lib/wallpaper/pool.ex
@@ -2,6 +2,32 @@ defmodule Wallpaper.Pool do
   require Logger
 
   @workers 8
+  @batch @workers*1000
+
+  def group_tasks(all_files, task_fn, batch \\ @batch) do
+    {next_tasks, result} = all_files
+    |> Stream.chunk_every(batch)
+    |> Enum.reduce({[], []}, fn files, {prev_tasks, result} ->
+      Logger.debug "prev_tasks #{length prev_tasks} files #{length files}"
+
+      new_tasks = Enum.map(files, fn file_path ->
+        Task.async(task_fn.(file_path))
+      end)
+      {cur_tasks, next_tasks} = Enum.split(new_tasks, div(batch, 2))
+      Logger.debug "new_tasks #{length new_tasks} prev_tasks #{length prev_tasks} next_tasks #{length next_tasks}"
+
+      result = [result, Task.await_many(prev_tasks, :infinity), Task.await_many(cur_tasks, :infinity)]
+
+      {next_tasks, result}
+    end)
+
+    if Enum.empty?(next_tasks) do
+      result
+    else
+      Logger.debug "next_tasks #{length next_tasks}"
+      [result, Task.await_many(next_tasks, :infinity)]
+    end
+  end
 
   def run(search_dir, result_dir) do
     Logger.info "Wallpaper.Pool.run"
@@ -22,13 +48,13 @@ defmodule Wallpaper.Pool do
 
     res = search_dir
     |> Wallpaper.find_files
-    |> Enum.map(fn {_size, file_path} ->
-        Task.async(fn ->
-          result_path = Path.join(result_dir, Path.relative_to(file_path, search_dir))
-          :poolboy.transaction(:image_worker, &( GenServer.call(&1, {:process, file_path, result_path}, :infinity) ), :infinity)
-        end)
-      end)
-    |> Task.await_many(:infinity)
+    |> Stream.map(fn {_size, file_path} -> file_path end)
+    |> group_tasks(fn file_path ->
+      fn ->
+        result_path = Path.join(result_dir, Path.relative_to(file_path, search_dir))
+        :poolboy.transaction(:image_worker, &( GenServer.call(&1, {:process, file_path, result_path}, :infinity) ), :infinity)
+      end
+    end)
 
     Supervisor.stop(super_pid, :normal)
 
--- test/wallpaper/pool_test.exs
+++ test/wallpaper/pool_test.exs
@@ -5,9 +5,16 @@ defmodule Wallpaper.PoolTest do
   @folder1 "priv/images"
   @result1 "priv/result"
 
+  test "group_tasks" do
+    res = Pool.group_tasks 100..110, fn n -> fn -> n*2 end end, 4
+    res = List.flatten(res)
+    pp res
+  end
+
   @tag timeout: :infinity
   test "run" do
     res = Pool.run @folder1, @result1
+    res = List.flatten(res)
     pp res
 
     assert length(res) > 10
